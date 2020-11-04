93c622302cea77335dcf79e3b6eb2001248ed43e
cetinirfan <56794844+cetinirfan@users.noreply.github.com>
2020-11-04 19:23:05 +0300

    Update mobx.js

--- mobx.js
+++ mobx.js
@@ -1,3 +1,9 @@
+npm i @react-native-community/async-storage --save
+react-native link
+
+
+
+MOBX--------------
 npm i mobx mobx-react --save
 npm i metro-react-native-babel-preset @babel/preset-flow @babel/plugin-proposal-decorators --save-dev
 
